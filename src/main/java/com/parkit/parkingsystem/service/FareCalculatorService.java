c0a6427b924eec6dcb5ea49eb080a8d2e8a70615
ChauNG <ngminhchauadressepro@gmail.com>
2024-10-10 23:40:31 +0200

    étape 3 implémentation de la fonctionnalité des 30min gratuites

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -5,17 +5,23 @@ import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
+
+	public void calculateFare(Ticket ticket){
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
-
+        		
         long inMillis = ticket.getInTime().getTime();
         long outMillis = ticket.getOutTime().getTime();
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
         double durationInHours = (double) (outMillis - inMillis) / (1000 * 60 * 60);
-
+        
+        if(durationInHours < 0.5){
+        	ticket.setPrice(0);
+        	return;
+        }
+        
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
                 ticket.setPrice(durationInHours * Fare.CAR_RATE_PER_HOUR);
